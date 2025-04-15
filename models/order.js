0ebda34afecb576cb4ee55fc39fd125e65c43d6d
Tapan Bhakhar <152768645+Tapan-Bhakhar@users.noreply.github.com>
2025-04-15 18:40:25 +0530

    Add Mongoose models for Order, Product, and User

---
+++ models/order.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    // userId: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User',
+    //     required: true
+    // },
+    // productId: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Product',
+    //     required: true
+    // },
+
+    userId: {
+        type: String,
+        required: true
+    },
+    products: [
+        {
+            productId: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                default: 1
+            }
+        }
+    ],
+
+    address: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        default: "pending",
+        // enum: ["pending", "confirmed", "shipped", "delivered", "cancelled"]
+        required: true
+    }
+
+}, { timestamps: true });
+
+export default mongoose.model('Order', OrderSchema);
